010852f5618782e1e6d084ea42520341bd95e034
Prathamesh2028 <166287425+Prathamesh2028@users.noreply.github.com>
2025-05-28 11:29:43 +0530

    Update product-details.jsx

--- client/src/components/shopping-view/product-details.jsx
+++ client/src/components/shopping-view/product-details.jsx
@@ -105,10 +105,11 @@ toast.error(`Only ${getQuantity} quantity can be added for this item`); // ✅ C
   return (
     <Dialog open={open} onOpenChange={handleDialogClose}>
       <DialogContent style={{
-  height: '700px',
+  height: '640px',
   overflow: 'scroll',
   scrollBehavior: 'smooth',
-  marginTop: '40px'
+  marginTop: '40px',
+      paddingBottom:'40px'
 }}
  className="grid grid-cols-2 gap-8 sm:p-12 max-w-[90vw] sm:max-w-[80vw] lg:max-w-[70vw]">
         <div className="relative overflow-hidden rounded-lg">
